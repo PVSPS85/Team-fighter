254d0e2ac71268aba36da31d6d6c35e755f558d7
nishanth-28-hub <nanth2810@gmail.com>
2026-04-06 09:41:14 +0530

    Update firebaseConfig.js

--- autoclinical-ai/src/src/firebase/firebaseConfig.js
+++ autoclinical-ai/src/src/firebase/firebaseConfig.js
@@ -1,1 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const storage = firebaseApp.storage();
+
+export { db, storage };
 
