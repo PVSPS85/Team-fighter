6a612afdeb75ca72a47cdf250870757f960ba566
PVSPS85 <pvsaipranav2007@gmail.com>
2026-04-06 10:20:05 +0530

    Update auth.js

--- autoclinical-ai/src/src/firebase/auth.js
+++ autoclinical-ai/src/src/firebase/auth.js
@@ -1,1 +1,21 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { app } from "./firebaseConfig";
 
+export const auth = getAuth(app);
+
+export const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logoutUser = () => {
+  return signOut(auth);
+};
+
+export const watchAuthState = (callback) => {
+  return onAuthStateChanged(auth, callback);
+};
