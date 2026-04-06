8d7d3efd76125373e99d6027927cb068ce390f55
harshithvaranasi <harshithvaranasi2218@gmail.com>
2026-04-06 09:29:37 +0530

    Update firebase.config.js

--- autoclinical-backend/functions/config/firebase.config.js
+++ autoclinical-backend/functions/config/firebase.config.js
@@ -1,1 +1,11 @@
+const admin = require("firebase-admin");
 
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+const db = admin.firestore();
+const storage = admin.storage();
+const auth = admin.auth();
+
+module.exports = { admin, db, storage, auth };
